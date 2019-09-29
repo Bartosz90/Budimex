c28eef4409113efe3f999d792a69f98d982d8fec
Bartek Krupa <krupa1.bk@gmail.com>
2019-09-29 22:13:34 +0200

    add sections

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,7 @@ window.onload = function() {
   }
   this.setTimeout(() => {
     nav.style.position = "fixed";
+    nav.style.zIndex = "3";
     main.style.position = "static";
   }, 3000);
 };
@@ -23,3 +24,34 @@ hamburger.addEventListener("click", toggleNav);
 function toggleNav(e) {
   nav.classList.toggle("active");
 }
+
+const navBtns = document.querySelectorAll(".navBtn");
+const sections = document.querySelectorAll(".mainSection");
+
+navBtns.forEach(btn => {
+  btn.addEventListener("click", e => {
+    toggleNav();
+    sections.forEach(section => {
+      const target = e.target.dataset.target;
+      if (
+        section.classList.value.includes("active") &&
+        !section.classList.value.includes(target)
+      ) {
+        section.classList.add("disappear");
+        setTimeout(() => section.classList.remove("active", "disappear"), 600);
+      }
+      if (
+        section.classList.value.includes(target) &&
+        !section.classList.value.includes("active")
+      ) {
+        section.classList.add("active");
+        navBtns.forEach(btn => {
+          btn.classList.remove("active");
+          if (btn.dataset.target.includes(target)) {
+            btn.classList.add("active");
+          }
+        });
+      }
+    });
+  });
+});
