5d20e38567354f5d9aaa833ededb3ea5ce86ef0a
Bartek Krupa <krupa1.bk@gmail.com>
2019-09-30 19:10:02 +0200

    add services section

--- scripts/script.js
+++ scripts/script.js
@@ -66,7 +66,49 @@ navBtns.forEach(btn => {
 const toggleTheme = document.querySelector(".toggleTheme");
 
 toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("light");
   sections.forEach(section => {
     section.classList.toggle("light");
+  });
+});
+
+// Services
+
+const serviceDescription = document.querySelector(".serviceDescription");
+const serviceTabs = document.querySelectorAll(".serviceBtn");
+
+const descriptions = [
+  "Zaplanujemy dla Ciebie każdy projekt, od deski do deski. Mamy w projektowaniu ogromne doświadczenie - możemy pochwalić się ponad 200 własnymi pracami oraz ponad 60 zrealizowanymi projektami dla klientów zewnętrznych.",
+  "Jesteśmy specjalistami we wznoszeniu nowych konstrukcji. W naszym portfolio budowniczym znajdują się zarówno małe projekty (domy jednorodzinne, krótkie odcinki dróg) jak i te największe - drapacze chmur, mosty zwodzone i inne.",
+  "Specjalizujemy się także w remontach - odnawiamy przede wszystkim domy i mieszkania, ale jesteśmy gotowi podjąć się renowacji także innych budynków.",
+  "Sprzedajemy hurtowo oraz detalicznie materiały budowlane z kategorii: chemia budowlana, fundamenty, ściany, poddasza, docieplenie, systemy kominowe oraz inne.",
+  "Sprzedajemy oraz wypożyczamy pojazdy i maszyny budowlane - między innymi dźwigi, betoniarki, koparki, kruszarki, spychacze, walce, wiertnice i wiele innych."
+];
+
+serviceDescription.textContent = descriptions[0];
+
+serviceTabs.forEach(tab => {
+  tab.addEventListener("click", e => {
+    const decsriptionIndex = descriptions.indexOf(
+      serviceDescription.textContent
+    );
+    if (decsriptionIndex !== Number(e.target.dataset.index)) {
+      serviceDescription.classList.remove("fade");
+      serviceDescription.textContent =
+        descriptions[Number(e.target.dataset.index)];
+      setTimeout(() => serviceDescription.classList.add("fade"), 100);
+    }
+
+    serviceTabs.forEach(tab => {
+      if (
+        tab.classList.value.includes("active") &&
+        tab.dataset.index !== e.target.dataset.index
+      ) {
+        tab.classList.remove("active");
+      }
+      if (tab.dataset.index === e.target.dataset.index) {
+        tab.classList.add("active");
+      }
+    });
   });
 });
